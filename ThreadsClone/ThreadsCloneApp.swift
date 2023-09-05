2c9a6b23620c52278ef5a1aede370378532cc4de
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-09-04 21:31:22 -0400

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Silvanei  Martins on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
